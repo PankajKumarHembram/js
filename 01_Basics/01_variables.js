e230765400a81f681a57a64f9c17306e462f0601
PankajKumarHembram <153085977+PankajKumarHembram@users.noreply.github.com>
2023-12-06 13:02:43 +0000

    Day 1 Learning

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const id = 1549
+let email = "pankaj@gmail.com"
+var city = "Mohali"
+state = "Punjab"
+
+let country ;
+
+// id=15  // Const value cannot be changed 
+
+console.log(id)
+
+console.table([id,email,city,state,country])
